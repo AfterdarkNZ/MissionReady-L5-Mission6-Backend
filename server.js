2ed2422ffde9b726984528dcbf0becb4911b6256
Kaitlyn Rohm <kaitlyn.j.rohm@gmail.com>
2024-06-24 12:27:12 +1200

    Basic set up

---
+++ server.js
@@ -0,0 +1,32 @@
+// ====== Packages and imports ====== //
+const express = require("express");
+const cors = require("cors"); // Import cors middleware once
+const app = express();
+require("dotenv").config();
+
+// Import routes
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// =========== ENDPOINTS =========== //
+// Initial setup in Postman
+app.get("/", (req, res) => {
+  console.log("Home endpoint reached");
+  res.send("Hello world");
+});
+
+// ============== PORT ============== //
+const PORT = process.env.PORT;
+app
+  .listen(PORT, () => {
+    console.log(`Server is alive on http://localhost:${PORT}`);
+  })
+  .on("error", (error) => {
+    if (error.code === "EADDRINUSE") {
+      console.log("PORT is already in use.");
+    } else {
+      console.log("Server Errors: ", error);
+    }
+  });
